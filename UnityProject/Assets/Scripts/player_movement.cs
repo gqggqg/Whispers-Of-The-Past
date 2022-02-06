bedd8fb6af128e6ce61c4afca46e2c209e72796f
BessonovMark <dima.160915@yandex.ru>
2022-02-06 19:27:16 +0200

    Add player movement script
    Added key bind to w, s, a, d keys.

---
+++ UnityProject/Assets/Scripts/player_movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    private float _velocity;
+    private Vector3 _direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _velocity = 0.3f;
+        _direction = new Vector3(0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) {
+            _direction.y = _direction.y + _velocity;
+        }
+        if (Input.GetKey(KeyCode.S)){
+            _direction.y = _direction.y - _velocity;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            _direction.x = _direction.x + _velocity;
+        }
+        if (Input.GetKey(KeyCode.A)){
+            _direction.x = _direction.x - _velocity;
+        }
+        Vector3.Normalize(_direction);
+        this.transform.position = this.transform.position + _direction;
+    }
+}
