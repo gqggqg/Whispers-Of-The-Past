e584895134f23ce8ed506c0a4eea0223dce6c895
BessonovMark <dima.160915@yandex.ru>
2022-02-06 21:18:42 +0200

    modify PlayerMovement script
    Changed from Update() to FixedUpdate()

--- UnityProject/Assets/Scripts/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -2,33 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player_movement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-
-    private float _velocity;
-    private Vector3 _direction;
+    [SerializeField]
+    private float _player_velocity;
+    private Vector3 _direction = new Vector3(0.0f, 0.0f);
     // Start is called before the first frame update
-    void Start()
-    {
-        _velocity = 0.3f;
-        _direction = new Vector3(0.0f, 0.0f);
+    void Start(){
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate(){
         _direction = Vector3.zero;
         if (Input.GetKey(KeyCode.W)) {
-            _direction.y = _direction.y + _velocity;
+            _direction.y = _direction.y + _player_velocity;
         }
         if (Input.GetKey(KeyCode.S)) {
-            _direction.y = _direction.y - _velocity;
+            _direction.y = _direction.y - _player_velocity;
         }
         if (Input.GetKey(KeyCode.D)) {
-            _direction.x = _direction.x + _velocity;
+            _direction.x = _direction.x + _player_velocity;
         }
         if (Input.GetKey(KeyCode.A)) {
-            _direction.x = _direction.x - _velocity;
+            _direction.x = _direction.x - _player_velocity;
         }
         Vector3.Normalize(_direction);
         this.transform.position = this.transform.position + _direction;
