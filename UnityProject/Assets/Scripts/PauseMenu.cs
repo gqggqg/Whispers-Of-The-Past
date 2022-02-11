ebc2d7030246174af919fd402036d8f5d1d0be25
ruzleer <gygygygyllll24@gmail.com>
2022-02-11 17:04:15 +0200

    Refactoring PauseMenu script

--- UnityProject/Assets/Scripts/PauseMenu.cs
+++ UnityProject/Assets/Scripts/PauseMenu.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI; //UI
 using UnityEngine.SceneManagement; //Scenes
 
 public class PauseMenu : MonoBehaviour {
-    private bool _pauseGame; //Check if game is paused
+
+    private bool _isGamePaused = false; //Check if game is paused
 
     [SerializeField]
     private GameObject _pauseGameMenu; //PauseMenu object
@@ -29,7 +30,7 @@ public class PauseMenu : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
+ /*   void Update() {
         if (Input.GetKey(KeyCode.Escape)) { //Using key ESC to open/close PauseMenu
             if (_pauseGame) { //If ESC pressed when game is paused the game continues
                 Resume();
@@ -38,17 +39,22 @@ public class PauseMenu : MonoBehaviour {
             }
         }
     }
+ */
 
     public void Resume() {
-        _pauseGameMenu.SetActive(false); //PauseMenu is inactive
-        Time.timeScale = 1f; //Time is in a regular regime
-        _pauseGame = false; //Game is not paused
-    }
-
-    public void Pause() {
-        _pauseGameMenu.SetActive(true); //PauseMenu is active
-        Time.timeScale = 0f; //Time is frozen
-        _pauseGame = true; //Game is paused
+ /*       if (Input.GetKeyDown(KeyCode.Escape)) { //Using key ESC to open/close PauseMenu
+            _isGamePaused = !_isGamePaused;
+            if (_isGamePaused) {
+                _pauseGameMenu.SetActive(true);
+                Time.timeScale = 0f;
+            } else { //If ESC pressed when game continues the game pauses
+                _pauseGameMenu.SetActive(false);
+                Time.timeScale = 1f;
+            }
+        } */
+          //     _pauseGameMenu.SetActive(false); //PauseMenu is inactive
+            //   Time.timeScale = 1f; //Time is in a regular regime
+               _isGamePaused = false; //Game is not paused
     }
 
     public void Settings() {
