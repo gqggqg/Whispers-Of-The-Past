0a5fd67b73b3aca737a63f41ff33f3b83815f36e
BessonovMark <dima.160915@yandex.ru>
2022-02-16 00:59:01 +0200

    Modify PlayerMovement by accident

--- UnityProject/Assets/Scripts/Player/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
+
     [SerializeField]
     private float _playerVelocity;
 
