e34e2f3030f20af9cf2acfa9d74822e86af75ec8
BessonovMark <dima.160915@yandex.ru>
2022-02-15 23:35:21 +0200

    Refactoring

--- UnityProject/Assets/Scripts/Player/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,28 +5,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float _player_velocity;
-    [SerializeField]
+    private float _playerVelocity;
+
     private Vector3 _direction;
 
     void Start(){
     }
 
     void FixedUpdate(){
+        ButtonMovement();
+    }
+
+    public void ButtonMovement() {
         _direction = Vector3.zero;
         if (Input.GetKey(KeyCode.W)) {
-            _direction.y = _direction.y + _player_velocity;
+            _direction.y = _direction.y + _playerVelocity;
         }
         if (Input.GetKey(KeyCode.S)) {
-            _direction.y = _direction.y - _player_velocity;
+            _direction.y = _direction.y - _playerVelocity;
         }
         if (Input.GetKey(KeyCode.D)) {
-            _direction.x = _direction.x + _player_velocity;
+            _direction.x = _direction.x + _playerVelocity;
         }
         if (Input.GetKey(KeyCode.A)) {
-            _direction.x = _direction.x - _player_velocity;
+            _direction.x = _direction.x - _playerVelocity;
         }
-        _direction = Vector3.Normalize(_direction) * _player_velocity;
+        _direction = Vector3.Normalize(_direction) * _playerVelocity;
         this.transform.position = this.transform.position + _direction;
     }
 }
