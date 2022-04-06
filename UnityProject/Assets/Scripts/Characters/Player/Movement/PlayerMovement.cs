b166f79dfad6dce6d3a8241fbe41abe4b68cd3db
BessonovMark <dima.160915@yandex.ru>
2022-04-06 20:50:51 +0200

    Modify Player_Movement script
    added flip feature to horizontal movement

--- UnityProject/Assets/Scripts/Characters/Player/Movement/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/Characters/Player/Movement/PlayerMovement.cs
@@ -7,25 +7,46 @@ namespace RPG {
 
         private Player_Manager _player_manager;
         private Rigidbody2D _rbody;
+        private Animator _player_animator;
 
         private float _player_speed;
         private Vector2 _direction;
+        [SerializeField]
+        public bool _facingRight;
 
         void Start() {
             _player_manager = GetComponent<Player_Manager>();
             _rbody = GetComponent<Rigidbody2D>();
-            _player_speed = _player_manager.baseMovementSpeed * Time.deltaTime;
+            _player_animator = GetComponent<Animator>();
             _direction = Vector2.zero;
+            _facingRight = false;
         }
 
 
         void Update() {
-            _direction.x = Input.GetAxisRaw("Horizontal");
-            _direction.y = Input.GetAxisRaw("Vertical");
+            _direction.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             _direction.Normalize();
+
+            _player_speed = _direction.magnitude * _player_manager.baseMovementSpeed;
+
+            if (_direction.x < 0 && _facingRight) {
+                Flip();
+            } 
+            else if (_direction.x > 0 && !_facingRight) {
+                Flip();
+            }
+            
+            _player_animator.SetFloat("speed", _player_speed);
         }
 
         void FixedUpdate() {
-            _rbody.MovePosition(_rbody.position + (_direction * _player_speed));        }
+            _rbody.MovePosition(_rbody.position + (_direction * _player_speed * Time.deltaTime));        
+        }
+
+        void Flip() {
+            _facingRight = !_facingRight;
+
+            transform.Rotate(0f, 180f, 0f);
+        }
     }
 }
