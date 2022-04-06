87bc3ed5c717905afb8eaeede780a02db57ab3fb
BessonovMark <dima.160915@yandex.ru>
2022-04-06 20:53:06 +0200

    Modify Character abstract class
    added facingRight property

--- UnityProject/Assets/Scripts/Characters/Character.cs
+++ UnityProject/Assets/Scripts/Characters/Character.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace RPG {
     public abstract class Character : MonoBehaviour {
+
+        public Animator characterAnimator;
+
         // Stats
         [SerializeField]
         public int maxHealth = 10;
@@ -15,5 +18,7 @@ namespace RPG {
 
         [SerializeField]
         public int baseMovementSpeed = 40;
+
+        private bool _facingRight;
     }
 }
