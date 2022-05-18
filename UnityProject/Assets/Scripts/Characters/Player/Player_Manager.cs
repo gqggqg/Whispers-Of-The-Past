a932423899d2e909c7016374caacd09a12c0163f
BessonovMark <dima.160915@yandex.ru>
2022-05-18 04:10:22 +0200

    Add shooting

--- UnityProject/Assets/Scripts/Characters/Player/Player_Manager.cs
+++ UnityProject/Assets/Scripts/Characters/Player/Player_Manager.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace RPG {
+
     public class Player_Manager : Character {
-        // Start is called before the first frame update
-        void Start() {
 
-        }
-
+        public KeyCode interactionKey = KeyCode.E;
+        public KeyCode attackKey = KeyCode.Mouse0;
+        public KeyCode switchWeaponKey = KeyCode.LeftShift;
+        /// ///////////////
+        /// 
+        public bool canAttack = false;
+    
         // Update is called once per frame
         void Update() {
 
