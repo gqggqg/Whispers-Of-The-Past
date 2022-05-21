9c3cf8ead8a19c404adb462b23972b5a325f8d6d
BessonovMark <dima.160915@yandex.ru>
2022-05-21 15:36:05 +0200

    Add shooting

--- UnityProject/Assets/Prefabs/HeavyBullet/HeavyBullet.cs
+++ UnityProject/Assets/Prefabs/HeavyBullet/HeavyBullet.cs
@@ -6,8 +6,10 @@ namespace RPG {
     public class HeavyBullet : MonoBehaviour {
         public float speed = 10f;
         public Rigidbody2D rb;
+        public Animator animator;
 
         void Start() {
+            animator = GetComponent<Animator>();
             Vector3 dir = Input.mousePosition;
             dir = Camera.main.ScreenToWorldPoint(dir);
             dir = dir - transform.position;
@@ -20,6 +22,24 @@ namespace RPG {
 
         private void OnTriggerEnter2D(Collider2D hitInfo) {
             Debug.Log(hitInfo.name);
+            Enemy target = hitInfo.GetComponent<Enemy>();
+
+            if (hitInfo.name == "HeavyBullet(Clone)") {
+                return;
+            }
+            if (hitInfo.name == "MainCharacter") {
+                return;
+            }
+            if (target != null) {
+                target.TakeDamage(1);
+            }
+            rb.velocity = Vector2.zero;
+            StartCoroutine(BulletCollapse());
+            
+        }
+        IEnumerator BulletCollapse() {
+            animator.SetBool("isDestroy", true);
+            yield return new WaitForSeconds(0.3f);
             Destroy(gameObject);
         }
 
