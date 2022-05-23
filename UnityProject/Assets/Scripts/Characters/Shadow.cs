8b79ee877d3ce54305fa7bdb8a3f699fc067da19
BessonovMark <dima.160915@yandex.ru>
2022-05-23 18:19:06 +0200

    Add Shadow, EnemyWeapon, ShadowWeapon scripts

---
+++ UnityProject/Assets/Scripts/Characters/Shadow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+namespace Game {
+    public class Shadow : Enemy {
+
+        
+
+        protected override void Start() {
+            base.Start();
+        }
+        private void Update() {
+           
+        }
+
+        
+    }
+}
