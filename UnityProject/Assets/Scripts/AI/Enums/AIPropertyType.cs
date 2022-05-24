472bbc1a03f3aea3f1d7dd90561e96646d486086
gqggqg <71404862+gqggqg@users.noreply.github.com>
2022-05-24 13:40:28 +0200

    Create AI enum files

---
+++ UnityProject/Assets/Scripts/AI/Enums/AIPropertyType.cs
@@ -0,0 +1,19 @@
+namespace Game.AI {
+
+    public enum AIPropertyType {
+
+        Hungry,
+        Hygiene,
+
+        Energy,
+        Social,
+
+        Money,
+
+        DistanceToFriend,
+        DistanceToEnemy,
+
+        RelationToReligion,
+        LoveToEat,
+    }
+}
